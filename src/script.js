c474a1e30cd36626a827d32d36bcbdb04d1b678f
Harold AO <36776278+haroldao@users.noreply.github.com>
2021-05-31 01:12:07 +0200

    Initial Commit

---
+++ src/script.js
@@ -0,0 +1,33 @@
+import './style.css';
+import * as THREE from 'three';
+
+// Scene
+const scene = new THREE.Scene();
+
+// Blue Cube
+const geometry =new THREE.BoxGeometry(1, 1, 1),
+      material = new THREE.MeshBasicMaterial({color: 'blue'}),
+      mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+//Sizes
+const sizes = {
+  width: 800,
+  height: 600
+};
+
+//Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 5;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector('canvas.scene');
+console.log(canvas)
+const renderer = new THREE.WebGLRenderer({
+  canvas
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
