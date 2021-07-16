84a1ef2634bcfee0e6939d514c09edffe0b888f2
Harold AO <36776278+haroldao@users.noreply.github.com>
2021-07-16 15:43:10 +0200

    Solution 2 use Clock

--- src/script.js
+++ src/script.js
@@ -1,7 +1,8 @@
 import './style.css'
 import * as THREE from 'three'
+import gsap from 'gsap'
 
-// Canvas
+
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
@@ -39,10 +40,10 @@ const tick = () => {
 
   const elapsedTime = clock.getElapsedTime()
 
-  // Update object
-  mesh.position.y = Math.sin(elapsedTime)
-  mesh.position.x = Math.cos(elapsedTime)
-  mesh.rotation.y = elapsedTime
+  // Update objects
+  camera.position.y = Math.sin(elapsedTime)
+  camera.position.x = Math.cos(elapsedTime)
+  camera.lookAt(mesh.position)
 
   // Render
   renderer.render(scene, camera)
