27fb697adaa780af44e6c47198dc9438a4657b05
Harold AO <36776278+haroldao@users.noreply.github.com>
2021-07-17 18:25:14 +0200

    .

--- src/script.js
+++ src/script.js
@@ -1,33 +1,57 @@
-import './style.css';
-import * as THREE from 'three';
+import './style.css'
+import * as THREE from 'three'
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
-// Blue Cube
-const geometry =new THREE.BoxGeometry(1, 1, 1),
-      material = new THREE.MeshBasicMaterial({color: 'blue'}),
-      mesh = new THREE.Mesh(geometry, material);
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
 
-scene.add(mesh);
-
-//Sizes
-const sizes = {
-  width: 800,
-  height: 600
-};
-
-//Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 5;
-scene.add(camera);
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.x = 2
+camera.position.y = 2
+camera.position.z = 2
+camera.lookAt(mesh.position)
+scene.add(camera)
 
 // Renderer
-const canvas = document.querySelector('canvas.scene');
-console.log(canvas)
 const renderer = new THREE.WebGLRenderer({
-  canvas
-});
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
 
-renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+// Animate
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    mesh.rotation.y = elapsedTime;
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
