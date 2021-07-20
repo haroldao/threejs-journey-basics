35c32e25763a611cefb87d30f4f86848d8869a51
Harold AO <36776278+haroldao@users.noreply.github.com>
2021-07-20 14:44:14 +0200

    Triangle

--- src/script.js
+++ src/script.js
@@ -11,9 +11,25 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Create an empty BufferGeometry
+const geometry = new THREE.BufferGeometry()
+
+const positionsArray = new Float32Array([
+    0,0,0,
+    0,1,0,
+    1,0,0
+])	
+
+// Create the attribute and name it 'position'
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute)
+
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
+const material = new THREE.MeshBasicMaterial({ 
+    color: 0xff0000,
+    wireframe: true
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
